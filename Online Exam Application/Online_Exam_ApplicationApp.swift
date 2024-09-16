0d17737d9ec3f11b682101bc8a8e1e86e007b8c9
FaridaaM <farida.mohamed13@msa.edu.eg>
2024-09-16 03:06:02 +0300

    Initial Commit

---
+++ Online Exam Application/Online_Exam_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_Exam_ApplicationApp.swift
+//  Online Exam Application
+//
+//  Created by Farida Mohamed on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Online_Exam_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
